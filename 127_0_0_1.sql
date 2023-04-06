8c577193d64c37f8013ac61e0aa47f0032f1c3dc
Maji1106 <121413493+Maji1106@users.noreply.github.com>
2023-04-06 20:11:55 +0700

    6/4/66

---
+++ 127_0_0_1.sql
@@ -0,0 +1,198 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: April 6, 2023 at 06:21 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `เตี๋ยวกับตำ`
+--
+CREATE DATABASE IF NOT EXISTS `เตี๋ยวกับตำ` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `เตี๋ยวกับตำ`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `cus_ID` char(6) NOT NULL,
+  `cus_name` varchar(30) NOT NULL,
+  `cus_table` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`cus_ID`, `cus_name`, `cus_table`) VALUES
+('CT 1', 'เปิ้ล', 22),
+('CT 2', 'เอก', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food_drink`
+--
+
+CREATE TABLE `food_drink` (
+  `F_D_ID` char(6) NOT NULL,
+  `F_Name` varchar(30) NOT NULL,
+  `F_price` int(4) NOT NULL,
+  `menu_ID` char(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `food_drink`
+--
+
+INSERT INTO `food_drink` (`F_D_ID`, `F_Name`, `F_price`, `menu_ID`) VALUES
+('D01', 'มะนาวโซดา', 40, '002'),
+('D02', 'นมชมพูเย็น', 40, '002'),
+('D03', 'ชาไทย', 40, '002'),
+('F01', 'เตี๋ยวแห้ง', 45, '001'),
+('F02', 'เตี๋ยวต้มส้ม', 45, '001'),
+('F03', 'ผัดซีอิ้วหมู', 50, '001');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `menu_ID` char(6) NOT NULL,
+  `menu_Name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`menu_ID`, `menu_Name`) VALUES
+('001', 'อาหาร'),
+('002', 'เครื่องดื่ม');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_ID` char(6) NOT NULL,
+  `cus_ID` char(6) NOT NULL,
+  `order_Date` date NOT NULL,
+  `order_Time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_ID`, `cus_ID`, `order_Date`, `order_Time`) VALUES
+('000001', 'CT 2', '2021-10-27', '09:09:00'),
+('000192', 'CT 1', '2023-03-12', '00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_detail`
+--
+
+CREATE TABLE `order_detail` (
+  `order_ID` char(6) NOT NULL,
+  `F_D_ID` char(6) NOT NULL,
+  `Quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `order_detail`
+--
+
+INSERT INTO `order_detail` (`order_ID`, `F_D_ID`, `Quantity`) VALUES
+('000192', 'F01', 1),
+('000192', 'F02', 1),
+('000192', 'F03', 1),
+('000001', 'F03', 1),
+('000192', 'D01', 1),
+('000192', 'D02', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`cus_ID`);
+
+--
+-- Indexes for table `food_drink`
+--
+ALTER TABLE `food_drink`
+  ADD PRIMARY KEY (`F_D_ID`),
+  ADD KEY `menu_ID` (`menu_ID`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`menu_ID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_ID`),
+  ADD KEY `cus_ID` (`cus_ID`);
+
+--
+-- Indexes for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD KEY `F_ID` (`F_D_ID`),
+  ADD KEY `order_ID` (`order_ID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `food_drink`
+--
+ALTER TABLE `food_drink`
+  ADD CONSTRAINT `food_drink_ibfk_1` FOREIGN KEY (`menu_ID`) REFERENCES `menu` (`menu_ID`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`cus_ID`) REFERENCES `customer` (`cus_ID`);
+
+--
+-- Constraints for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD CONSTRAINT `order_detail_ibfk_1` FOREIGN KEY (`F_D_ID`) REFERENCES `food_drink` (`F_D_ID`),
+  ADD CONSTRAINT `order_detail_ibfk_2` FOREIGN KEY (`order_ID`) REFERENCES `orders` (`order_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css"> 
+        <link rel="stylesheet" href="style.css">
+        <title>CRUD Food Information111</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12"> <br>
+                    <h3>รายชื่อลูกค้า <a href="addFood_dropdown.php" class="btn btn-info float-end">+เพิ่มข้อมูล</a> </h3>
+                    <table class="table table-striped  table-hover table-responsive table-bordered">
+                        <thead align="center">                        
+                            <tr>
+                                <th width="10%">รหัสอาหาร</th>
+                                <th width="20%">อาหารและเครื่องดื่ม</th>
+                                <th width="20%">ราคา</th>
+                                <th width="25%">ประเภท</th>
+                                <th width="10%">ภาพ</th>
+                                <th width="5%">แก้ไข</th> 
+                                <th width="5%">ลบ</th>
+                            </tr>                       
+                        </thead>
+
+                        <tbody>
+                          <?php
+                            require 'connect.php';
+                            $sql =  "SELECT * FROM food_drink,menu WHERE food_drink.menu_ID = menu.menu_ID";
+                            $stmt = $conn->prepare($sql);
+                            $stmt->execute();
+                            $result = $stmt->fetchAll();
+                            foreach ($result as $r) { ?>
+                            <tr>
+                                <td><?= $r['F_D_ID'] ?></td>
+                                <td><?= $r['F_Name'] ?></td>
+                                <td><?= $r['F_price'] ?></td>
+                                <td><?= $r['menu_ID'] ?></td>
+                                <td><img src="./img/<?= $r['Image']; ?>" width="50px" height="50" alt="image" onclick="enlargeImg()" id="img1" ></td>
+                                <td><a href="updatecustomerForm.php?F_D_ID=<?= $r['F_D_ID'] ?>" class="btn btn-warning btn-sm">แก้ไข</a></td>
+                                <td><a href="deletecustomer.php?F_D_ID=<?= $r['F_D_ID'] ?>" class="btn btn-danger btn-sm" onclick="return confirm('ยืนยันการลบข้อมูล !!');">ลบ</a></td>
+                            </tr>
+                          <?php 
+                            $conn = null;}
+                          ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>  
+        
+    <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#customerTable').DataTable();
+        });
+    </script> 
+    </body>
+</html>
